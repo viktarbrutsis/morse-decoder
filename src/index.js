d4e5c0b396bc40374d77c2b56dd2fa4c1588fdf7
Viktar <viktarbrutsis@gmail.com>
2023-01-23 12:17:30 +0300

    feat: morse done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,64 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let res = [], newRes = [], newresFinal = [], final = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        res.push(expr.slice(i, i + 10));
+    }
+    //console.log(res);
+  
+    let j = 0;
+  
+    res.forEach(item => {
+        newRes[j] = '';
+        for (let i = 0; i < item.length; i = i + 2) {
+            // newRes[j] = '';
+            if((item.slice(i, i + 2)) == '10' || (item.slice(i, i + 2)) == '11' || (item.slice(i, i + 2)) == '**') {
+  
+                newRes[j] = newRes[j] + item.slice(i, i + 2);
+            }
+        }
+        j++;
+    });
+  
+    //console.log(newRes);
+  
+    j = 0;
+  
+    newRes.forEach(item => {
+        newresFinal[j] = '';
+        for (let i = 0; i < item.length; i = i + 2) {
+            // newRes[j] = '';
+            if (item == '**********') {
+                newresFinal[j] = ' ';
+                // console.log(newresFinal[j].length);
+          
+            } else if (item.slice(i, i + 2) == '10') {
+                newresFinal[j] = newresFinal[j] + '.';
+         
+            } else if (item.slice(i, i + 2) == '11') {
+                newresFinal[j] = newresFinal[j] + '-';
+           
+            }
+        }
+        j++;
+    });
+  
+    //console.log(newresFinal);
+  
+    for (let i = 0; i < newresFinal.length; i++) {
+      if(newresFinal[i] === ' ') {
+        final = final + ' ';
+      } else {
+        final = final + MORSE_TABLE[newresFinal[i]];
+      }
+    }
+  
+    //console.log(final);
+    return final;
+  
+  }
 
 module.exports = {
     decode
-}
+};
